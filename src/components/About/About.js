4dc6d7b09b3a38b349940850eaebce624e9b3278
shadmansaalim321 <shadmansaalim321@gmail.com>
2021-09-30 22:25:10 +0600

    404 Page Done

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -3,7 +3,7 @@ import React from 'react';
 const About = () => {
     return (
         <div style={{ marginTop: '120px', color: 'white', width: '60%', marginRight: 'auto', marginLeft: 'auto' }}>
-            <p><i>React Application Developed By <strong style={{ color: 'steelblue' }}>Saalim Shadman</strong>, A Computer Science Student at RMIT Australia. I used React and Material-UI to build this Application.This is my first project using Material-UI. I am trying to explore and learn new things.</i></p>
+            <p style={{ fontSize: '24px' }}><i>React Application Developed By <strong style={{ color: 'steelblue' }}>Saalim Shadman</strong>, A Computer Science Student at RMIT Australia. I used React and Material-UI to build this Application.This is my first project using Material-UI. I am trying to explore and learn new things.</i></p>
         </div>
     );
 };
--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,9 +1,11 @@
 import React from 'react';
-
+import logo from '../../logo.svg'
 const NotFound = () => {
     return (
-        <div>
-            <h2>Not Found Page</h2>
+        <div style={{ marginTop: '200px', color: 'white' }}>
+            <h3>404 Error</h3>
+            <h1 style={{ fontSize: '48px' }}>Page Not Found</h1>
+            <img className="App-logo" src={logo} alt="" />
         </div>
     );
 };
